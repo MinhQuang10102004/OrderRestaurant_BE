03aa22e81750c5375a08e4847864e8eecc397a1f
Nguyen Tan Dat <tandat081104@gmail.com>
2026-03-28 00:14:53 +0700

    feat: CRUD categories

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -22,19 +22,19 @@ export class CategoryController {
   @Post()
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
-  @ApiOperation({ summary: 'Create a new category (Admin/Staff)' })
+  @ApiOperation({ summary: 'Tạo danh mục mới(Admin/Staff)' })
   create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoryService.create(createCategoryDto);
   }
 
   @Get()
-  @ApiOperation({ summary: 'Get all categories' })
+  @ApiOperation({ summary: 'Lấy tất cả danh mục' })
   findAll() {
     return this.categoryService.findAll();
   }
 
   @Get(':id')
-  @ApiOperation({ summary: 'Get a category by ID' })
+  @ApiOperation({ summary: 'Lấy danh mục theo ID' })
   findOne(@Param('id') id: string) {
     return this.categoryService.findOne(BigInt(id));
   }
@@ -42,7 +42,7 @@ export class CategoryController {
   @Patch(':id')
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
-  @ApiOperation({ summary: 'Update a category (Admin/Staff)' })
+  @ApiOperation({ summary: 'Cập nhật danh mục(Admin/Staff)' })
   update(
     @Param('id') id: string,
     @Body() updateCategoryDto: UpdateCategoryDto,
@@ -53,7 +53,7 @@ export class CategoryController {
   @Delete(':id')
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
-  @ApiOperation({ summary: 'Soft delete a category (Admin)' })
+  @ApiOperation({ summary: 'Soft delete danh mục(Admin)' })
   remove(@Param('id') id: string) {
     return this.categoryService.remove(BigInt(id));
   }
